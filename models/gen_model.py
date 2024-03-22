0047bbac486531668c3db1c9500d4fd7bcf3299b
AdriPad99 <adrian.mpadilla99@gmail.com>
2024-03-22 18:58:32 -0500

    comnt fixs

--- models/gen_model.py
+++ models/gen_model.py
@@ -1,24 +1,33 @@
 from app import db
 
+#Model for the ps generations resource
 class gen_Model(db.Model):
+
+    #sets the table name
     __tablename__ = 'ps_generations'
+
+    #sets the outline for the database
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(25), nullable = False, unique = True)
     price = db.Column(db.Integer, nullable = False)
     global_release_date = db.Column(db.Integer, nullable = False, unique = False)
 
+    #adds and commits local information
     def add_gen(self):
         db.session.add(self)
         db.session.commit()
 
+    #saves local information to the database
     def save_gen(self):
         db.session.add(self)
         db.session.commit()
 
+    #deletes information from the database
     def del_gen(self):
         db.session.delete(self)
         db.session.commit()
 
+    #forms the requests for the database
     def from_gen(self, dict):
         for k , v in dict.items():
             setattr(self, k, v)
--- models/ygo_model.py
+++ models/ygo_model.py
@@ -1,7 +1,12 @@
 from app import db
 
+#Model for the ygo card resource
 class ygo_Model(db.Model):
+
+    #sets table name
     __tablename__ = 'fav_cards'
+
+    #sets the outline for the database table
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(42), nullable = False, unique = True) #unique
     effect = db.Column(db.String, nullable = False, unique = True) #unique
@@ -14,18 +19,22 @@ class ygo_Model(db.Model):
     defense = db.Column(db.Integer, nullable = True)
     summon_requirement = db.Column(db.String, nullable = True)
 
+    #adds and commits local information
     def add_ygo(self):
         db.session.add(self)
         db.session.commit()
 
+    #saves local information to the database
     def save_ygo(self):
         db.session.add(self)
         db.session.commit()
 
+    #deletes information from the database
     def del_ygo(self):
         db.session.delete(self)
         db.session.commit()
 
+    #forms the requests for the database
     def from_ygo(self, dict):
         for k , v in dict.items():
             setattr(self, k, v)
